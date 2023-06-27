be4f8f9b7b2b76c6455932ea02a5062be2c429d1
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-06-27 16:54:18 +0200

    checkpoint milestone 4

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,53 +16,50 @@ export default {
 }
 </script>
 
-<template >
+<template>
     <div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="film">
-                        <ul>
-                            <li class="" v-for="(film, index) in store.filmsObj" :key="index">
-                                <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" alt="">
-                                <div class="film d-flex">
-                                    <div class="me-3">{{ film.original_title }}</div>
-                                    <img class="cntr-flag"
-                                        :src="`../../node_modules/country-flag-icons/1x1/${film.original_language.toUpperCase()}.svg`"
-                                        :alt="film.original_language.toUpperCase()">
-                                    <!--<div class="me-3">{{ film.vote_average }}</div>-->
-                                    <div>
-                                        <span>
-                                            Voto:&nbsp;
-                                            <i v-for="star in Math.round(film.vote_average / 2)" :key="star" class="fa-solid fa-star" style="color: #e9e316"></i>
-                                            <i v-for="star in Math.round(5 - film.vote_average / 2)" :key="star" class="fa-regular fa-star" style="color: #e9e316"></i>
-                                        </span>
-                                    </div>
-                                    
-                                </div>
-                            </li>
-                        </ul>
+      <div class="container">
+            <div class="film">
+              <div class="row">
+                <li class="col-4" v-for="(film, index) in store.filmsObj" :key="index">
+                  <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" alt="">
+                  <div class="film d-flex">
+                    <div class="me-3">{{ film.original_title }}</div>
+                    <img class="cntr-flag"
+                      :src="`../../node_modules/country-flag-icons/1x1/${film.original_language.toUpperCase()}.svg`"
+                      :alt="film.original_language.toUpperCase()">
+                    <div>
+                      <span>
+                        Voto:&nbsp;
+                        <i v-for="star in Math.round(film.vote_average / 2)" :key="star" class="fa-solid fa-star" style="color: #e9e316"></i>
+                        <i v-for="star in Math.round(5 - film.vote_average / 2)" :key="star" class="fa-regular fa-star" style="color: #e9e316"></i>
+                      </span>
                     </div>
-
-                    <div class="tv">
-                        <ul>
-                            <li v-for="(tv, index) in store.tvsObj" :key="index">
-                                <div class="tv d-flex">
-                                    <img :src="`https://image.tmdb.org/t/p/w342/${tv.poster_path}`" alt="">
-                                    <div class="me-3">{{ tv.name }}</div>
-                                    <div class="me-3">{{ tv.original_name }}</div>
-                                    <img class="cntr-flag"
-                                    :src="`../../node_modules/country-flag-icons/1x1/${tv.original_language.toUpperCase()}.svg`"
-                                    :alt="tv.original_language.toUpperCase()">
-                                    <div class="me-3">{{ tv.vote_average }}</div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                  </div>
+                </li>
+              </div>
+            </div>
+            <!-- visualizzazione serie tv -->
+            <div class="tv">
+                <div class="row"> <!--RIPRENDERE A LAVORARE DA QUESTO PEZZO-->
+                    <li class="col-4" v-for="(tv, index) in store.tvsObj" :key="index">
+                        <div class="tv d-flex">
+                            <img :src="`https://image.tmdb.org/t/p/w342/${tv.poster_path}`" alt="">
+                            <div class="serie">
+                                <div class="me-3">{{ tv.name }}</div>
+                                <div class="me-3">{{ tv.original_name }}</div>
+                                <img class="cntr-flag"
+                                :src="`../../node_modules/country-flag-icons/1x1/${tv.original_language.toUpperCase()}.svg`"
+                                :alt="tv.original_language.toUpperCase()">
+                                <div class="me-3">{{ tv.vote_average }}</div>
+                                <!--fine serie-->
+                            </div>
+                        </div>
+                    </li>
                 </div>
             </div>
+          </div>
         </div>
-    </div>
 </template>
 
 <style lang="scss" scoped>
